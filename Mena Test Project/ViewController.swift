50a57f2916a01fd1309b583e68572c67cc6e724c
Pete Barnes <pete_barnes@me.com>
2017-12-20 00:54:07 -0500

    feat: Added code to play notes.

--- Mena Test Project/ViewController.swift
+++ Mena Test Project/ViewController.swift
@@ -9,11 +9,77 @@
 import UIKit
 import AudioKitUI
 
-class ViewController: UIViewController {
+enum PianoKey: Int {
+    case C4
+    case CS4
+    case D4
+    case DS4
+    case E4
+    case F4
+    case FS4
+    case G4
+    case GS4
+    case A4
+    case AS4
+    case B4
+    case C5
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var cButton: UIButton!
+    @IBOutlet weak var cSharpButton: UIButton!
+    @IBOutlet weak var dButton: UIButton!
+    @IBOutlet weak var dSharpButton: UIButton!
+    @IBOutlet weak var eButton: UIButton!
+    @IBOutlet weak var fButton: UIButton!
+    @IBOutlet weak var fSharpButton: UIButton!
+    @IBOutlet weak var gButton: UIButton!
+    @IBOutlet weak var gSharpButton: UIButton!
+    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var aSharpButton: UIButton!
+    @IBOutlet weak var bButton: UIButton!
+    @IBOutlet weak var highCButton: UIButton!
+    
+    var oscillator = AKOscillator(waveform: AKTable(.sawtooth))
+    var currentAmplitude = 0.1
+    var currentRampTime = 0.2
+    
+    var frequencies = [PianoKey.C4: 261.626,
+                 PianoKey.CS4: 277.183,
+                 PianoKey.D4: 293.665,
+                 PianoKey.DS4: 311.127,
+                 PianoKey.E4: 329.628,
+                 PianoKey.F4: 349.228,
+                 PianoKey.FS4: 369.994,
+                 PianoKey.G4: 391.995,
+                 PianoKey.GS4: 415.305,
+                 PianoKey.A4: 440.000,
+                 PianoKey.AS4: 466.164,
+                 PianoKey.B4: 493.883,
+                 PianoKey.C5: 523.251]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let oscMixer = AKMixer(oscillator)
+        
+        let reverb = AKReverb(oscMixer)
+        reverb.loadFactoryPreset(.largeHall)
+        reverb.dryWetMix = 0.5
+        
+        let tape = try! AKAudioFile()
+        
+        let player = try! AKAudioPlayer(file: tape)
+        
+        let mixer = AKMixer(player, reverb)
+        AudioKit.output = mixer
+        
+        AudioKit.start()
+        
+        let recorder = try! AKNodeRecorder(node: oscMixer, file: tape)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,5 +88,30 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func keyTouchUp(_ sender: Any) {
+        noteOff()
+    }
+    
+    @IBAction func keyTouchDown(_ sender: UIButton) {
+        noteOn(note: PianoKey(rawValue: sender.tag) ?? PianoKey.C4)
+    }
+    
+    // Helper Functions
+    func noteOn(note: PianoKey) {
+        // start from the correct note if amplitude is zero
+        if oscillator.amplitude == 0 {
+            oscillator.rampTime = 0
+        }
+        oscillator.frequency = frequencies[note] ?? 0
+        
+        // Still use rampTime for volume
+        oscillator.rampTime = currentRampTime
+        oscillator.amplitude = currentAmplitude
+        oscillator.play()
+    }
+    
+    func noteOff() {
+        oscillator.amplitude = 0
+    }
 }
 
